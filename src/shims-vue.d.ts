64f7aee244931b48977e89ddf30fc9b632844bd1
mgdewet <malan.de.wet@gudat-solutions.com>
2024-09-13 13:19:28 +0200

    standardisation

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,11 +1,11 @@
 declare namespace JSX {
-    interface IntrinsicElements {
-      [elem: string]: any
-    }
+  interface IntrinsicElements {
+    [elem: string]: any
   }
-  
-  declare module '*.vue' {
-    import type { DefineComponent } from 'vue'
-    const component: DefineComponent<{}, {}, any>
-    export default component
-  }
+}
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue"
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
